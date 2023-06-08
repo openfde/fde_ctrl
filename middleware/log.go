700f501d71005cc7da84736fefe44ebc44acf749
gongyong1734 <185457686@qq.com>
2023-06-08 16:58:34 +0800

    correct code

--- middleware/log.go
+++ middleware/log.go
@@ -10,7 +10,7 @@ func LogHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 		if len(c.Errors) > 0 {
-			logger.Error(c.Request.Context(), "", "middle_ware_errors", c.Errors.ByType(gin.ErrorTypePrivate).String(), c.Errors[0])
+			logger.Error(c.Request.Context(), "middle_ware_errors", c.Errors.ByType(gin.ErrorTypePrivate).String(), c.Errors[0])
 			return
 		}
 		logger.Info(c.Request.Context(), "middle_ware", sourceWithCtx(c))
