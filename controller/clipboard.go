8080c9a16489e056d528488b8d59aacbf3ad7a12
gongyong1734 <185457686@qq.com>
2023-06-13 16:20:18 +0800

    add api to read clipboard

--- controller/clipboard.go
+++ controller/clipboard.go
@@ -18,6 +18,7 @@ type ClipboardImpl struct {
 func (impl ClipboardImpl) Setup(r *gin.RouterGroup) {
 	v1 := r.Group("/v1")
 	v1.POST("/clipboard", impl.WriteHandler)
+	v1.GET("/clipboard", impl.ReadHandler)
 }
 
 type wirteClipBoardRequest struct {
@@ -31,6 +32,23 @@ const (
 	txtFormat   = clipboardFormat("text")
 	imageFormat = clipboardFormat("image")
 )
+
+func (impl ClipboardImpl) ReadHandler(c *gin.Context) {
+	formatQuery := c.Query("Format")
+	var formatClip clipboard.Format
+	switch {
+	case formatQuery == string(imageFormat):
+		{
+			formatClip = clipboard.FmtImage
+		}
+	default:
+		{
+			formatClip = clipboard.FmtText
+		}
+	}
+	data := clipboard.Read(formatClip)
+	response.Response(c, data)
+}
 
 func (impl ClipboardImpl) WriteHandler(c *gin.Context) {
 	var request wirteClipBoardRequest
