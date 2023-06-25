6bc55c558bb206a3c3b2672debaf74ba0c2da084
gongyong1734 <185457686@qq.com>
2023-06-25 17:44:12 +0800

    add debug

--- controller/display.go
+++ controller/display.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"fde_ctrl/logger"
 	"fde_ctrl/response"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 
@@ -18,10 +21,31 @@ func (impl DisplayManager) Setup(r *gin.RouterGroup) {
 
 func (impl DisplayManager) mirrorHandler(c *gin.Context) {
 	cmd := exec.Command("xrandr", "--output DP-1", "--auto")
-	cmd.Env = append(os.Environ())
+	cmd.Env = os.Environ()
+	var stdout, stderr io.ReadCloser
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		logger.Error("stdout pipe for vnc server", nil, err)
+		return
+	}
+	stderr, err = cmd.StderrPipe()
+	if err != nil {
+		logger.Error("stdout pipe for vnc server", nil, err)
+		return
+	}
 	cmd.Run()
+	output, err := ioutil.ReadAll(io.MultiReader(stdout, stderr))
+	if err != nil {
+		logger.Error("read start vnc server failed", nil, err)
+	}
+	logger.Info("debug_xrandr_auto", string(output))
 	cmd = exec.Command("xrandr", "--output DP-1", "--same-as eDP-1")
-	cmd.Env = append(os.Environ())
+	cmd.Env = os.Environ()
 	cmd.Run()
+	output, err = ioutil.ReadAll(io.MultiReader(stdout, stderr))
+	if err != nil {
+		logger.Error("read start vnc server failed", nil, err)
+	}
+	logger.Info("debug_xrandr_mirror", string(output))
 	response.Response(c, nil)
 }
