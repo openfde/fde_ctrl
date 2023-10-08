f89fc0853e6d8cf267e2e6f9b56d9fda70d94570
gongyong <185457686@qq.com>
2023-10-08 11:35:07 +0800

    add a unit test

---
+++ fs_fusion/util_test.go
@@ -0,0 +1,36 @@
+package fs_fusion
+
+import "testing"
+
+func Test_validPermR(t *testing.T) {
+	type args struct {
+		uid  uint32
+		duid uint32
+		gid  uint32
+		dgid uint32
+		perm uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "owner ",
+			args: args{
+				uid:  1000,
+				duid: 1000,
+				gid:  1000,
+				dgid: 1000,
+				perm: 16889,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validPermR(tt.args.uid, tt.args.duid, tt.args.gid, tt.args.dgid, tt.args.perm); got != tt.want {
+				t.Errorf("validPermR() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
