f62199c11185656db19310b18b60cec4818061c4
gongyonog <185457686@qq.com>
2023-10-08 15:36:24 +0800

    correct member name for linux

--- fs_fusion/linux.go
+++ fs_fusion/linux.go
@@ -45,9 +45,9 @@ func copyFusestatFromGostat(dst *fuse.Stat_t, src *syscall.Stat_t) {
 	dst.Gid = uint32(src.Gid)
 	dst.Rdev = uint64(src.Rdev)
 	dst.Size = int64(src.Size)
-	dst.Atim.Sec, dst.Atim.Nsec = src.Atimespec.Sec, src.Atimespec.Nsec
-	dst.Mtim.Sec, dst.Mtim.Nsec = src.Mtimespec.Sec, src.Mtimespec.Nsec
-	dst.Ctim.Sec, dst.Ctim.Nsec = src.Ctimespec.Sec, src.Ctimespec.Nsec
+	dst.Atim.Sec, dst.Atim.Nsec = src.Atim.Sec, src.Atim.Nsec
+        dst.Mtim.Sec, dst.Mtim.Nsec = src.Mtim.Sec, src.Mtim.Nsec
+        dst.Ctim.Sec, dst.Ctim.Nsec = src.Ctim.Sec, src.Ctim.Nsec
 	dst.Blksize = int64(src.Blksize)
 	dst.Blocks = int64(src.Blocks)
 }
