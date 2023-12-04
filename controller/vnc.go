20ddb605b4b7a2ca9e5f95a3ea83ff46149d9017
gongyong <185457686@qq.com>
2023-12-04 18:40:07 +0800

    Expand the number to 10000000

--- controller/vnc.go
+++ controller/vnc.go
@@ -251,7 +251,7 @@ func (impl VncAppImpl) startVncApp(app, path string, sysOnly bool) (port string,
 	}
 	var arg []string
 	arg = append(arg, "--SecurityTypes=None", "-name="+app, "--I-KNOW-THIS-IS-INSECURE",
-		"-BlacklistThreshold=10000", "-BlacklistTimeout=0")
+		"-BlacklistThreshold=10000000", "-BlacklistTimeout=0")
 	logger.Info("app_not_start", app)
 	if !sysOnly {
 		err = constructXstartup(app, path)
