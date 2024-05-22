cd1bfbe0c0e77efd66adf775425701b5961f4ba3
gongyong <185457686@qq.com>
2024-05-22 16:39:46 +0800

    only checking cur_gl when the powervr.ini exist

--- gpu/x100.go
+++ gpu/x100.go
@@ -34,9 +34,12 @@ func  IsReady() (bool,error){
 			lists := strings.Split(line,"=")
 			if len(lists) == 2 {
 				if strings.Contains(lists[1],"phytium_display_pci") {
-					_,err = os.Stat("/dev/cur_gl")
-					if err != nil {
-						return false,errors.New("cur_gl not found")
+					_,err = os.Stat("/etc/powervr.ini") //only kylin os has powervr.ini
+					if err == nil {
+						_,err = os.Stat("/dev/cur_gl")
+						if err != nil {
+							return false,errors.New("cur_gl not found")
+						}
 					}
 				}
 			}
