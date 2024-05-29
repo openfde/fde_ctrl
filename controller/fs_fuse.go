68232b9b2b22554c37b7b517ea6ef553cb54d7f0
185457686 <185457686@qq.com>
2024-05-29 16:37:52 +0800

    optimize personal fs fusing

--- controller/fs_fuse.go
+++ controller/fs_fuse.go
@@ -4,10 +4,7 @@ import (
 	"fde_ctrl/logger"
 	"fde_ctrl/response"
 	"io/ioutil"
-	"net/http"
-	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"sync"
 
@@ -59,8 +56,16 @@ func (impl FsFuseManager) getHandler(c *gin.Context) {
 	return
 }
 
-func mountFdePtfs(sourcePath, targetPath string) error {
-	cmd := exec.Command("fde_ptfs", "-o", "nonempty", "-o", "allow_other", sourcePath, targetPath)
+func mountFdePtfs() error {
+	cmd := exec.Command("fde_fs", "-pm")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func umountFdePtfs() error {
+	cmd := exec.Command("fde_fs", "-pu")
 	err := cmd.Run()
 	if err != nil {
 		return err
@@ -73,75 +78,13 @@ type mountInfo struct {
 	Target string
 }
 
-var homeDirNameMap map[string]string
-var androidDirList, linuxDirList []string
-
-func init() {
-	homeDirNameMap = make(map[string]string)
-	// Initialize the map with key-value pairs
-	homeDirNameMap["Documents"] = "文档"
-	homeDirNameMap["Downloads"] = "下载"
-	homeDirNameMap["Music"] = "音乐"
-	homeDirNameMap["Videos"] = "视频"
-	homeDirNameMap["Pictures"] = "图片"
-	linuxDirList = append(linuxDirList, "Documents")
-	linuxDirList = append(linuxDirList, "Downloads")
-	linuxDirList = append(linuxDirList, "Music")
-	linuxDirList = append(linuxDirList, "Videos")
-	linuxDirList = append(linuxDirList, "Pictures")
-
-	androidDirList = append(androidDirList, "Documents")
-	androidDirList = append(androidDirList, "Download")
-	androidDirList = append(androidDirList, "Music")
-	androidDirList = append(androidDirList, "Movies")
-	androidDirList = append(androidDirList, "Pictures")
-}
-
-func getUserFolders() ([]string, []string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, nil, err
-	}
-	var realLinuxDirList = make([]string, len(linuxDirList))
-	var realAndroidList = make([]string, len(androidDirList))
-	_, err = os.Stat(filepath.Join(homeDir, linuxDirList[0]))
-	if err == nil { //en
-		for i, v := range linuxDirList {
-			realLinuxDirList[i] = filepath.Join(homeDir, v)
-			realAndroidList[i] = filepath.Join(homeDir, ".local/share/openfde/media/0", androidDirList[i])
-		}
-	} else { //zh
-		for i, v := range linuxDirList {
-			realLinuxDirList[i] = filepath.Join(homeDir, homeDirNameMap[v])
-			realAndroidList[i] = filepath.Join(homeDir, ".local/share/openfde/media/0", androidDirList[i])
-		}
-	}
-	return realLinuxDirList, realAndroidList, nil
-
-}
-
-var fsExit chan string
-
 func (impl FsFuseManager) setHandler(c *gin.Context) {
 	if get() {
 		response.Response(c, nil)
 		return
 	}
-	list, androidList, err := getUserFolders()
-	if err != nil {
-		logger.Error("get_user_folders", nil, err)
-		response.ResponseError(c, http.StatusInternalServerError, err)
-		return
-	}
 	go func() {
-		for i, v := range list {
-			logger.Info("mount_fde_ptfs", list[i]+" "+androidList[i])
-			go mountFdePtfs(v, androidList[i])
-		}
-		select {
-		case <-fsExit:
-			return
-		}
+		mountFdePtfs()
 	}()
 
 	response.Response(c, nil)
@@ -149,7 +92,7 @@ func (impl FsFuseManager) setHandler(c *gin.Context) {
 
 func FsFusingExit() {
 	if get() {
-		fsExit <- "exit"
+		umountFdePtfs()
 		return
 	}
 }
