4655f8ff56a272e6fe75acfd4b97f33950420e42
185457686 <185457686@qq.com>
2024-08-07 13:56:39 +0800

    grep fde-renderer first,if exists then do nothing and return

--- gpu/jm9100.go
+++ gpu/jm9100.go
@@ -7,6 +7,17 @@ type JM9100 struct {
 
 func (gpu JM9100) IsReady() (bool, error) {
 	//start fde-renderer
-	exec.Command("fde-renderer").Run()
+	// Check if fde-renderer process is already running
+	cmd := exec.Command("pgrep", "fde-renderer")
+	if err := cmd.Run(); err == nil {
+		// fde-renderer process is already running
+		return true, nil
+	}
+
+	// Start fde-renderer process
+	if err := exec.Command("fde-renderer").Start(); err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
