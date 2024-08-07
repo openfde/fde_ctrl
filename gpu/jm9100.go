0f0cecfa0ca18a468954c361dbddcbe3d5aa9d1b
185457686 <185457686@qq.com>
2024-08-07 14:23:37 +0800

    call fde_fs -s to set softmode on kylin

--- gpu/jm9100.go
+++ gpu/jm9100.go
@@ -1,6 +1,9 @@
 package gpu
 
-import "os/exec"
+import (
+	"os/exec"
+	"time"
+)
 
 type JM9100 struct {
 }
@@ -13,10 +16,23 @@ func (gpu JM9100) IsReady() (bool, error) {
 		// fde-renderer process is already running
 		return true, nil
 	}
+	// Run fde_fs -s command
+	if err := exec.Command("fde_fs", "-s").Run(); err != nil {
+		return false, err
+	}
 
 	// Start fde-renderer process
 	if err := exec.Command("fde-renderer").Start(); err != nil {
-		return false, err
+		// Check again if fde-renderer process is already running using a loop
+		for i := 0; i < 10; i++ {
+			cmd := exec.Command("pgrep", "fde-renderer")
+			if err := cmd.Run(); err == nil {
+				// fde-renderer process is already running
+				return true, nil
+			}
+			time.Sleep(time.Second) // Wait for 1 second before checking again
+		}
+		return false, nil
 	}
 
 	return true, nil
