9136cf3f71415dbea4609969d3d3c723b91f4890
185457686 <185457686@qq.com>
2025-06-10 11:39:44 +0800

    add new member for android apps

--- controller/android_app.go
+++ controller/android_app.go
@@ -1,24 +1,58 @@
 package controller
 
 import (
+	"fde_ctrl/logger"
 	"fde_ctrl/response"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 type AndroidApp struct {
-	Name        string
+	Name        string `json:"name"`
 	PackageName string
+	IconPath    string `json:"icon"`   // path to the icon file
+	Path        string `json:"path"`   // how to launch the app fde_launch com.android.app
+	Uninstll    string `json:"uninst"` // how to uninstall the app fde_uninstall com.android.app
 }
+
+var fdeAppIconBaseDir = ".local/share/openfde/icons"
 
 type AndroidApps []AndroidApp
 
 func (impl AndroidApp) Setup(r *gin.RouterGroup) {
 	v1 := r.Group("/v1")
 	v1.GET("/android/apps", impl.AppsHandler)
+}
+
+func scanAppInfo(lines []string, home string) AndroidApps {
+	var appsList AndroidApps
+	var app AndroidApp
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, "Name: ") {
+			app.Name = strings.TrimPrefix(line, "Name: ")
+		} else if strings.HasPrefix(line, "packageName: ") {
+			app.PackageName = strings.TrimPrefix(line, "packageName: ")
+			app.IconPath = filepath.Join(home, fdeAppIconBaseDir, app.PackageName+".png")
+			app.Uninstll = "waydroid app remove" + app.PackageName
+			app.Path = "fde_launch " + app.PackageName
+			logger.Info("scan_android", app.PackageName)
+			_, err := os.Stat(app.IconPath) // check if the icon file exists
+			if err != nil && os.IsNotExist(err) {
+				logger.Error("stat_android_icon", app.PackageName, err)
+				app = AndroidApp{}
+				continue
+			}
+			appsList = append(appsList, app)
+			app = AndroidApp{}
+		}
+	}
+	return appsList
 }
 
 func (impl AndroidApp) AppsHandler(c *gin.Context) {
@@ -28,20 +62,13 @@ func (impl AndroidApp) AppsHandler(c *gin.Context) {
 		response.ResponseError(c, http.StatusInternalServerError, err)
 		return
 	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		response.ResponseError(c, http.StatusInternalServerError, err)
+		return
+	}
 
 	lines := strings.Split(strings.TrimSpace(string(output)), "\n")
-	var appsList AndroidApps
-	var app AndroidApp
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "Name: ") {
-			app.Name = strings.TrimPrefix(line, "Name: ")
-		} else if strings.HasPrefix(line, "packageName: ") {
-			app.PackageName = strings.TrimPrefix(line, "packageName: ")
-			appsList = append(appsList, app)
-			app = AndroidApp{}
-		}
-	}
-	response.Response(c, appsList)
+	response.Response(c, scanAppInfo(lines, home))
 	return
 }
